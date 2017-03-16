9738134559dda916ad424739a38365abcbe01e4d
Mohammed Zubair Ahmed <12032337@cqumail.com>
2017-03-15 20:26:23 -0500

    change of the unit code

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -21,7 +21,7 @@ public class SimpleServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        response.getWriter().print("Hello World!");
+        response.getWriter().print("COIT20260");
     }
 
 }
